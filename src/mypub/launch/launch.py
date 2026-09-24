eca7adad9c40bbb1feb790bd705e7334598f8c71
david-erm <64047317+david-erm@users.noreply.github.com>
2026-09-24 14:36:46 -0500

    solution updates

--- src/mypub/launch/launch.py
+++ src/mypub/launch/launch.py
@@ -3,13 +3,13 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     publisher_node = Node(
-        package='mypub',
-        executable='publisher_node',
+        package='my_package',
+        executable='my_publisher_node',
     )
 
     listener_node = Node(
-        package='mypub',
-        executable='listener_node',
+        package='my_package',
+        executable='my_listener_node',
     )
 
     return LaunchDescription([publisher_node, listener_node])
